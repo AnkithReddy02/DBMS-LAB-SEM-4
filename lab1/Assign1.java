8d8c6a2ba76d5c894be53787c8c5b2f8a6fc2d1b
Ankith Reddy <ankithreddy2001@gmail.com>
2021-01-07 10:45:51 +0530

    Create Assign1.java

---
+++ lab1/Assign1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Assign1
+{
+
+	public static void main(String[] args) throws Exception
+	{
+
+		Questiona.a();
+		Questionb.b();
+		Questionc.c();
+		Questiond.d();
+	    
+
+	}
+
+}
